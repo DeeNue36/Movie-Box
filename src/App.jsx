4e015bf9f32fd940cca92833049fe06ee4125031
Dugiii <dugidemeg@gmail.com>
2026-01-08 22:48:36 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>
+        Movie Box!
+        </h1>
+    </div>
+  )
+}
+
+export default App
