d59191edb672fedb5df6e167a1957ec35bccbeef
Dugiii <dugidemeg@gmail.com>
2026-01-08 23:40:19 -0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>
-        Movie Box!
-        </h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="container">
+        <header>
+          <img src="../public/hero-img.png" alt="Hero Banner" />
+          <h1>
+            Find <span className='text-gradient'>Movies</span> You Love To Watch Without the Hassle
+          </h1>
+
+        </header>
+      </div>
+    </main>
   )
 }
 
