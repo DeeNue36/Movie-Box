2cb4995410c0e0d9ffb64a924e660f3862c11475
Dugiii <dugidemeg@gmail.com>
2026-01-14 18:45:55 -0600

    imported api objects

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -2,18 +2,8 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { Spinner } from './Spinner'
+import { API_BASE_URL, API_OPTIONS } from '../api'
 
-
-const API_BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
-const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${API_KEY}`
-    }
-}
 
 export const MovieDetails = () => {
     const { id } = useParams();
